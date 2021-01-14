f1677f67b69950958168eb99ffc5859a103e1b65
nezuu2 <nezuu_pc2@yahoo.co.jp>
2021-01-14 16:33:33 +0900

    aタグのボタンにもイベントを設定

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 $(function(){
   $("#check-jquery").text("このサイトにはJQueryが導入されています。");
-  $("#num").text("btn_click");
 
   $("#button").on('click', function() {
     $("#text").text("buttonが押されました。");
@@ -8,5 +7,9 @@ $(function(){
 
   $("#btn").on('click', function() {
     alert("作者のTwitterのリンクへ飛びます。\nお問い合わせはDMへお願いします。");
+  });
+
+  $("#button-a").on('click', function() {
+    alert("click!");
   });
 });
